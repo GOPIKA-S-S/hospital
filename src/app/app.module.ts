d2acf392e2e925a2e54d683264d6d628c8d86d61
GOPIKA-S-S <gopikass1024@gmail.com>
2022-07-07 20:49:09 +0530

    search-department completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrontviewComponent } from './frontview/frontview.component';
+import { SearchDepartmentComponent } from './search-department/search-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FrontviewComponent
+    FrontviewComponent,
+    SearchDepartmentComponent
   ],
   imports: [
     BrowserModule,
