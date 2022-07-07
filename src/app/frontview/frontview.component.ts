34b551eb7e3c6071bf6e882f19824054b012fc78
GOPIKA-S-S <gopikass1024@gmail.com>
2022-07-07 20:33:17 +0530

    front-view completed

---
+++ src/app/frontview/frontview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-frontview',
+  templateUrl: './frontview.component.html',
+  styleUrls: ['./frontview.component.css']
+})
+export class FrontviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
